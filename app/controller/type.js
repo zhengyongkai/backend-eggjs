fbe862756e05627a4fad5e4fdbdb1c961d08ef16
zhengyongkai <825947557@qq.com>
2022-02-27 11:50:00 +0800

    分页提交

--- app/controller/type.js
+++ app/controller/type.js
@@ -1,19 +1,27 @@
 'use strict';
 
-
 const Controller = require('egg').Controller;
 
 class TypeController extends Controller {
   async query() {
-    const { ctx, app } = this;
+    const { ctx } = this;
+    const params = ctx.query;
     // 账单的相关参数，这里注意要把账单的 id 也传进来
+    const limit = params.limit ? params.limit : 10;
+    const page = params.page ? params.page : 1;
+    delete params.limit;
+    delete params.page;
     try {
-      const result = await ctx.service.type.query();
+      const pageList = await ctx.service.type.queryCount();
+      const result = await ctx.service.type.query(params, limit, page - 1);
       ctx.body = {
         code: 200,
         msg: '请求成功',
         data: {
           list: result,
+          total: pageList.length,
+          page,
+          pages: Math.ceil(pageList.length / limit),
         },
       };
     } catch (error) {
@@ -24,7 +32,6 @@ class TypeController extends Controller {
       };
     }
   }
-
 }
 
 module.exports = TypeController;
--- app/service/type.js
+++ app/service/type.js
@@ -3,12 +3,25 @@
 const Service = require('egg').Service;
 
 class TypeService extends Service {
-  async query() {
-    const { ctx, app } = this;
-    const QUERY_STR = '*';
-    const sql = `select ${QUERY_STR} from type `;
+  async queryCount() {
+    const { app } = this;
     try {
-      const result = await app.mysql.query(sql);
+      const result = app.mysql.select('type');
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+  async query(params, limit = 10, offset = 0) {
+    const { app } = this;
+    try {
+      limit = parseInt(limit);
+      const result = app.mysql.select('type', {
+        where: params,
+        limit,
+        offset: offset * limit,
+      });
       return result;
     } catch (error) {
       console.log(error);
